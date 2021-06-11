b6f9f4f24b7f9a070478b3eaf6db9507f5e2cec3
Santhoshnani12 <nanisanthosh789@gmail.com>
2021-06-11 11:33:41 +0530

    latest commit

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function sidebarShow() {
 function sidebarHide() {
   navLinks.style.right = "-200px";
 }
+
+
+// cursor style
+
+var cursor = document.getElementById("cursor")
+
+document.onmousemove = function(e) {
+  cursor.style.left = (e.pageX - 25) + "px";
+  cursor.style.top = (e.pageY - 25) + "px";
+  cursor.style.display = "block";
+}
